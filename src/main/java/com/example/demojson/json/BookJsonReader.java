fd3007d041f12bcc197af292969ee73016fd5311
aditidarandale06 <97491495+aditidarandale06@users.noreply.github.com>
2023-08-01 14:46:47 -0400

    Adding comparator method

--- src/main/java/com/example/demojson/json/BookJsonReader.java
+++ src/main/java/com/example/demojson/json/BookJsonReader.java
@@ -2,7 +2,12 @@ package com.example.demojson.json;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BookJsonReader {
@@ -33,6 +38,35 @@ public class BookJsonReader {
 		book.setTitle("Java 11 working");
 		System.out.println("Book title:" +book.getTitle());
 		System.out.println("Book title:" +book.getYear());
+		
+		//Reading 2 file that is list of json entries 
+		File booksjsonfile =new File("src/main/resources/books.json"); 
+		booksjsonfile.getPath();
+		
+		try {
+			List<Book> books = objectMapper.readValue(booksjsonfile, new TypeReference<List<Book>>() {});
+			System.out.println(Arrays.asList(books));
+			for(Book b : books) {
+				System.out.println(b.getTitle());
+			}
+			
+			System.out.println("After sorting --------");
+			
+			Collections.sort(books, new Comparator<Book>() {
+			    public int compare(Book book1, Book book2) {
+			        return book1.getTitle().compareTo(book2.getTitle());
+			    }
+			});
+			
+			for(Book b : books) {
+				System.out.println(b.getTitle());
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	
 	}
